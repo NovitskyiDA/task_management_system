2fb13e1f0f1f9467940a2b766b915bc0aacb2161
NovitskyiDA <allfayer@gmail.com>
2016-02-04 21:41:51 +0200

    fix email activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'task-management-system.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://task-management-system.herokuapp.com/' }
   ActionMailer::Base.smtp_settings = {
       :address        => "smtp.sendgrid.net",
       :port           => "25",
